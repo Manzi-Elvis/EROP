1380322066eeab86358393bc00a794cc963aa562
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-20 22:15:11 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
-function App() {
+import { Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/Dashboard.tsx"
 
+function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   )
 }
 
