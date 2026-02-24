844688204ee8ef528e99a3858859797809cb0a83
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-24 22:27:42 +0200

    🔴 Red → OVERLOADED 🟡 Yellow → UNDERUTILIZED 🟢 Green → OPTIMAL

--- src/components/UtilizationChart.tsx
+++ src/components/UtilizationChart.tsx
@@ -5,15 +5,28 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
-  CartesianGrid
+  CartesianGrid,
+  Cell
 } from "recharts"
-import { type UserUtilization } from "../types/User"
+import { UserUtilization } from "../types/User"
 
 interface Props {
   data: UserUtilization[]
 }
 
 export default function UtilizationChart({ data }: Props) {
+  
+  const getColor = (status: string) => {
+    switch (status) {
+      case "OVERLOADED":
+        return "#ef4444" // red
+      case "UNDERUTILIZED":
+        return "#f59e0b" // yellow
+      default:
+        return "#10b981" // green
+    }
+  }
+
   return (
     <div style={{ width: "100%", height: 400 }}>
       <ResponsiveContainer>
@@ -22,7 +35,11 @@ export default function UtilizationChart({ data }: Props) {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Bar dataKey="total_allocation" />
+          <Bar dataKey="total_allocation">
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={getColor(entry.status)} />
+            ))}
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
