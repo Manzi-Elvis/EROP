d01e7dff9055d344ce48ab0fc40d90d05a4b3e75
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-24 22:23:12 +0200

    Utilization Chart Component

---
+++ src/components/UtilizationChart.tsx
@@ -0,0 +1,30 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid
+} from "recharts"
+import { UserUtilization } from "../types/User"
+
+interface Props {
+  data: UserUtilization[]
+}
+
+export default function UtilizationChart({ data }: Props) {
+  return (
+    <div style={{ width: "100%", height: 400 }}>
+      <ResponsiveContainer>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="total_allocation" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
